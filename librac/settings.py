47ade00db60d387e9f5d6734abfdb4c6aeec6fe5
Zettex <safaandsafa4@gmail.com>
2024-10-05 12:14:21 +0600

    db added

--- librac/settings.py
+++ librac/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-_&b0!uu$!1*z+*xu57u&#atxfu5=wxk0r#vl6-bkwg**33c-mg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1",".vercel.app"]
 
 
 # Application definition
@@ -81,7 +81,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'librac.wsgi.application'
+WSGI_APPLICATION = 'librac.wsgi.app'
 
 
 # Database
@@ -143,6 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
